4cfc1905fdb2b6d0330bd2045877c387dfff5dd4
Serchinastico <54cymru@gmail.com>
2018-11-26 23:47:57 +0100

    Adapt older tests to use the new API

--- rules/src/test/java/com/serchinastico/rules/NoDataFrameworksFromAndroidClassDetectorTest.kt
+++ rules/src/test/java/com/serchinastico/rules/NoDataFrameworksFromAndroidClassDetectorTest.kt
@@ -1,138 +1,95 @@
 package com.serchinastico.rules
 
-import com.android.tools.lint.checks.infrastructure.LintDetectorTest.java
-import com.android.tools.lint.checks.infrastructure.LintDetectorTest.kotlin
-import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import com.serchinastico.rules.test.LintTest
+import com.serchinastico.rules.test.LintTest.Expectation.NoErrors
+import com.serchinastico.rules.test.LintTest.Expectation.SomeError
 import org.junit.Test
 
-class NoDataFrameworksFromAndroidClassDetectorTest {
+class NoDataFrameworksFromAndroidClassDetectorTest : LintTest {
+
+    override val issue = NoDataFrameworksFromAndroidClassDetector.ISSUE
+
     @Test
     fun inJavaNonAndroidClass_whenFileHasFrameworkImport_detectsNoErrors() {
-        lint()
-            .files(
-                java(
-                    """
-                        |package foo;
-                        |
-                        |import com.squareup.retrofit2.*;
-                    """.trimMargin()
-                )
-            )
-            .issues(NoDataFrameworksFromAndroidClassDetector.ISSUE)
-            .run()
-            .expectClean()
+        expect(
+            """
+                |package foo;
+                |
+                |import com.squareup.retrofit2.*;
+            """.trimMargin()
+        ).inJava toHave NoErrors
     }
 
     @Test
     fun inJavaAndroidClass_whenFileHasNoFrameworkImport_detectsNoErrors() {
-        lint()
-            .files(
-                java(
-                    """
-                        |package foo;
-                        |
-                        |import android.app.Activity;
-                        |import android.os.Bundle;
-                        |import android.view.View;
-                        |
-                        |public class TestActivity extends Activity {}
-                    """.trimMargin()
-                )
-            )
-            .issues(NoDataFrameworksFromAndroidClassDetector.ISSUE)
-            .run()
-            .expectClean()
+        expect(
+            """
+                |package foo;
+                |
+                |import android.app.Activity;
+                |import android.os.Bundle;
+                |import android.view.View;
+                |
+                |public class TestClass extends Activity {}
+            """.trimMargin()
+        ).inJava toHave NoErrors
     }
 
     @Test
     fun inJavaAndroidClass_whenFileHasFrameworkImport_detectsErrors() {
-        lint()
-            .files(
-                java(
-                    """
-                        |package foo;
-                        |
-                        |import android.app.Activity;
-                        |import android.os.Bundle;
-                        |import android.view.View;
-                        |import com.squareup.retrofit2.*;
-                        |
-                        |public class TestActivity extends Activity {}
-                    """.trimMargin()
-                )
-            )
-            .issues(NoDataFrameworksFromAndroidClassDetector.ISSUE)
-            .run()
-            .expect(
-                """
-                    |src/foo/TestActivity.java: Error: Framework classes to get or store data should never be called from Activities, Fragments or any other Android related view. [NoDataFrameworksFromAndroidClass]
-                    |1 errors, 0 warnings
-                """.trimMargin()
-            )
+        expect(
+            """
+                |package foo;
+                |
+                |import android.app.Activity;
+                |import android.os.Bundle;
+                |import android.view.View;
+                |import com.squareup.retrofit2.*;
+                |
+                |public class TestClass extends Activity {}
+            """.trimMargin()
+        ).inJava toHave SomeError
     }
 
     @Test
     fun inKotlinNonAndroidClass_whenFileHasFrameworkImport_detectsNoErrors() {
-        lint()
-            .files(
-                kotlin(
-                    """
-                        |package foo
-                        |
-                        |import com.squareup.retrofit2.*
-                    """.trimMargin()
-                )
-            )
-            .issues(NoDataFrameworksFromAndroidClassDetector.ISSUE)
-            .run()
-            .expectClean()
+        expect(
+            """
+                |package foo
+                |
+                |import com.squareup.retrofit2.*
+            """.trimMargin()
+        ).inKotlin toHave NoErrors
     }
 
     @Test
     fun inKotlinAndroidClass_whenFileHasNoFrameworkImport_detectsNoErrors() {
-        lint()
-            .files(
-                kotlin(
-                    """
-                        |package foo
-                        |
-                        |import android.app.Activity
-                        |import android.os.Bundle
-                        |import android.view.View
-                        |
-                        |class TestActivity : Activity()
-                    """.trimMargin()
-                )
-            )
-            .issues(NoDataFrameworksFromAndroidClassDetector.ISSUE)
-            .run()
-            .expectClean()
+        expect(
+            """
+                |package foo
+                |
+                |import android.app.Activity
+                |import android.os.Bundle
+                |import android.view.View
+                |
+                |class TestClass : Activity()
+            """.trimMargin()
+        ).inKotlin toHave NoErrors
     }
 
     @Test
     fun inKotlinAndroidClass_whenFileHasFrameworkImport_detectsErrors() {
-        lint()
-            .files(
-                kotlin(
-                    """
-                        |package foo
-                        |
-                        |import android.app.Activity
-                        |import android.os.Bundle
-                        |import android.view.View
-                        |import com.squareup.retrofit2.*;
-                        |
-                        |class TestActivity : Activity()
-                    """.trimMargin()
-                )
-            )
-            .issues(NoDataFrameworksFromAndroidClassDetector.ISSUE)
-            .run()
-            .expect(
-                """
-                    |src/foo/TestActivity.kt: Error: Framework classes to get or store data should never be called from Activities, Fragments or any other Android related view. [NoDataFrameworksFromAndroidClass]
-                    |1 errors, 0 warnings
-                """.trimMargin()
-            )
+        expect(
+            """
+                |package foo
+                |
+                |import android.app.Activity
+                |import android.os.Bundle
+                |import android.view.View
+                |import com.squareup.retrofit2.*;
+                |
+                |class TestClass : Activity()
+            """.trimMargin()
+        ).inKotlin toHave SomeError
     }
 }
--- rules/src/test/java/com/serchinastico/rules/NoPrintStackTraceCallsDetectorTest.kt
+++ rules/src/test/java/com/serchinastico/rules/NoPrintStackTraceCallsDetectorTest.kt
@@ -1,158 +1,105 @@
 package com.serchinastico.rules
 
-import com.android.tools.lint.checks.infrastructure.LintDetectorTest.java
-import com.android.tools.lint.checks.infrastructure.LintDetectorTest.kotlin
-import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import com.serchinastico.rules.test.LintTest
+import com.serchinastico.rules.test.LintTest.Expectation.NoErrors
+import com.serchinastico.rules.test.LintTest.Expectation.SomeError
 import org.junit.Test
 
-class NoPrintStackTraceCallsDetectorTest {
+class NoPrintStackTraceCallsDetectorTest : LintTest {
+
+    override val issue = NoPrintStackTraceCallsDetector.ISSUE
+
     @Test
     fun inJavaNonThrowableClass_whenCallIsPrintStackTrace_detectsNoErrors() {
-        lint()
-            .files(
-                java(
-                    """
-                        |package foo;
-                        |
-                        |class TestClass {
-                        |   public static void main(String[] args) {
-                        |       new TestClass().printStackTrace();
-                        |   }
-                        |
-                        |   private void printStackTrace() {}
-                        |}
-                    """.trimMargin()
-                )
-            )
-            .issues(NoPrintStackTraceCallsDetector.ISSUE)
-            .run()
-            .expectClean()
+        expect(
+            """
+                |package foo;
+                |
+                |class TestClass {
+                |   public static void main(String[] args) {
+                |       new TestClass().printStackTrace();
+                |   }
+                |
+                |   private void printStackTrace() {}
+                |}
+            """.trimMargin()
+        ).inJava toHave NoErrors
     }
 
     @Test
     fun inJavaThrowableChildClass_whenCallIsPrintStackTrace_detectsErrors() {
-        lint()
-            .files(
-                java(
-                    """
-                        |package foo;
-                        |
-                        |class TestClass extends java.lang.Throwable {
-                        |   public static void main(String[] args) {
-                        |       new TestClass().printStackTrace();
-                        |   }
-                        |}
-                    """.trimMargin()
-                )
-            )
-            .issues(NoPrintStackTraceCallsDetector.ISSUE)
-            .run()
-            .expect(
-                """
-                    |src/foo/TestClass.java: Error: There should not be calls to the printStackTrace method in Throwable instances [NoPrintStackTraceCalls]
-                    |1 errors, 0 warnings
-                """.trimMargin()
-            )
+        expect(
+            """
+                |package foo;
+                |
+                |class TestClass extends java.lang.Throwable {
+                |   public static void main(String[] args) {
+                |       new TestClass().printStackTrace();
+                |   }
+                |}
+            """.trimMargin()
+        ).inJava toHave SomeError
     }
 
     @Test
     fun inJavaThrowableClass_whenCallIsPrintStackTrace_detectsErrors() {
-        lint()
-            .files(
-                java(
-                    """
-                        |package foo;
-                        |
-                        |class TestClass {
-                        |   public static void main(String[] args) {
-                        |       new java.lang.Throwable().printStackTrace();
-                        |   }
-                        |}
-                    """.trimMargin()
-                )
-            )
-            .issues(NoPrintStackTraceCallsDetector.ISSUE)
-            .run()
-            .expect(
-                """
-                    |src/foo/TestClass.java: Error: There should not be calls to the printStackTrace method in Throwable instances [NoPrintStackTraceCalls]
-                    |1 errors, 0 warnings
-                """.trimMargin()
-            )
+        expect(
+            """
+                |package foo;
+                |
+                |class TestClass {
+                |   public static void main(String[] args) {
+                |       new java.lang.Throwable().printStackTrace();
+                |   }
+                |}
+            """.trimMargin()
+        ).inJava toHave SomeError
     }
 
     @Test
     fun inKotlinNonThrowableClass_whenCallIsPrintStackTrace_detectsNoErrors() {
-        lint()
-            .files(
-                kotlin(
-                    """
-                        |package foo;
-                        |
-                        |class TestClass {
-                        |   fun main(args: Array<String>) {
-                        |       TestClass().printStackTrace();
-                        |   }
-                        |
-                        |   private fun printStackTrace() {}
-                        |}
-                    """.trimMargin()
-                )
-            )
-            .issues(NoPrintStackTraceCallsDetector.ISSUE)
-            .run()
-            .expectClean()
+        expect(
+            """
+                |package foo
+                |
+                |class TestClass {
+                |   fun main(args: Array<String>) {
+                |       TestClass().printStackTrace();
+                |   }
+                |
+                |   private fun printStackTrace() {}
+                |}
+            """.trimMargin()
+        ).inKotlin toHave NoErrors
     }
 
     @Test
     fun inKotlinThrowableChildClass_whenCallIsPrintStackTrace_detectsErrors() {
-        lint()
-            .files(
-                kotlin(
-                    """
-                        |package foo;
-                        |
-                        |class TestClass: kotlin.Throwable() {
-                        |   fun main(args: Array<String>) {
-                        |       TestClass().printStackTrace();
-                        |   }
-                        |}
-                    """.trimMargin()
-                )
-            )
-            .issues(NoPrintStackTraceCallsDetector.ISSUE)
-            .run()
-            .expect(
-                """
-                    |src/foo/TestClass.kt: Error: There should not be calls to the printStackTrace method in Throwable instances [NoPrintStackTraceCalls]
-                    |1 errors, 0 warnings
-                """.trimMargin()
-            )
+        expect(
+            """
+                |package foo
+                |
+                |class TestClass: kotlin.Throwable() {
+                |   fun main(args: Array<String>) {
+                |       TestClass().printStackTrace();
+                |   }
+                |}
+            """.trimMargin()
+        ).inKotlin toHave SomeError
     }
 
     @Test
     fun inKotlinThrowableClass_whenCallIsPrintStackTrace_detectsErrors() {
-        lint()
-            .files(
-                kotlin(
-                    """
-                        |package foo;
-                        |
-                        |class TestClass {
-                        |   fun main(args: Array<String>) {
-                        |       kotlin.Throwable().printStackTrace();
-                        |   }
-                        |}
-                    """.trimMargin()
-                )
-            )
-            .issues(NoPrintStackTraceCallsDetector.ISSUE)
-            .run()
-            .expect(
-                """
-                    |src/foo/TestClass.kt: Error: There should not be calls to the printStackTrace method in Throwable instances [NoPrintStackTraceCalls]
-                    |1 errors, 0 warnings
-                """.trimMargin()
-            )
+        expect(
+            """
+                |package foo
+                |
+                |class TestClass {
+                |   fun main(args: Array<String>) {
+                |       kotlin.Throwable().printStackTrace();
+                |   }
+                |}
+            """.trimMargin()
+        ).inKotlin toHave SomeError
     }
 }
