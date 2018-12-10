008862fa5e06ac528fa43fa599474fa3e67bff50
Serchinastico <54cymru@gmail.com>
2018-12-10 20:33:44 +0100

    Fix jacoco and missing dependency:

--- app/src/main/java/com/serchinastico/lin/TestActivity.kt
+++ app/src/main/java/com/serchinastico/lin/TestActivity.kt
@@ -1,20 +1,3 @@
 package com.serchinastico.lin
 
-import android.view.View
-
-class TestClass {
-    public val view: View = View(null)
-
-    fun main() {
-        val asd: Asd = Asd.valueOf("Asd")
-        when (asd) {
-            Asd.Asd -> print("asd")
-            Asd.Bsd -> print("bsd")
-            else -> print("csd")
-        }
-    }
-
-    enum class Asd {
-        Asd, Bsd, Csd
-    }
-}
+class TestClass
