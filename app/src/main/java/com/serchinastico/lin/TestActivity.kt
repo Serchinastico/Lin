00fe56a8dcb93ed817b6cd483fc806da3c825465
Serchinastico <54cymru@gmail.com>
2018-12-10 20:20:40 +0100

    Configure gradle to make the whole dsl and rules projects work

--- app/src/main/java/com/serchinastico/lin/TestActivity.kt
+++ app/src/main/java/com/serchinastico/lin/TestActivity.kt
@@ -4,4 +4,17 @@ import android.view.View
 
 class TestClass {
     public val view: View = View(null)
+
+    fun main() {
+        val asd: Asd = Asd.valueOf("Asd")
+        when (asd) {
+            Asd.Asd -> print("asd")
+            Asd.Bsd -> print("bsd")
+            else -> print("csd")
+        }
+    }
+
+    enum class Asd {
+        Asd, Bsd, Csd
+    }
 }
